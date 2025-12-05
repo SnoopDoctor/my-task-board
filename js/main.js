3e4db61c65662d9458c7a51f2de247faa5f048f0
SnoopDoctor <151868678+SnoopDoctor@users.noreply.github.com>
2025-12-05 19:38:17 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-// Основной файл приложения - точка входа
 document.addEventListener('DOMContentLoaded', function() {
-    
-    // Инициализируем приложение с задержкой для гарантии загрузки DOM
     setTimeout(() => {
         initializeBoard();
     }, 100);
