9d26672c55207647b9415d56ff7aa8c1e918a87b
SnoopDoctor <151868678+SnoopDoctor@users.noreply.github.com>
2025-12-15 19:55:53 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-window.FIREBASE_CONFIG = {
-    apiKey: "AIzaSyAO6C8t_h5YIXEgjpnPB7Uqbn9uU99vKeQ",
-    authDomain: "kanban-board-ff058.firebaseapp.com",
-    projectId: "kanban-board-ff058",
-    storageBucket: "kanban-board-ff058.firebasestorage.app",
-    messagingSenderId: "546700858700",
-    appId: "1:546700858700:web:413b9cc999987ae8ed2be9",
-    measurementId: "G-KNE80WS3ML"
-};
